b8e73d0432462310f6c39b77ab5abde61d05555a
s schmitt <smschm@umich.edu>
2012-05-28 18:29:22 -0400

    changed syntax for rule.m

--- rule.m
+++ rule.m
@@ -1,42 +1,42 @@
-function s = rule(varargin)
-
-% states:
-% 0 = waiting for rule name
-% 1 = expecting a : or ;
-% 2 = in parameter list (takes a rule name or ;)
-state = 0;
+function rule(varargin)
 
 rn = '';
 deplist = {};
+fdlist = {};
+rule = '';
 
-for arg = varargin
-    arg = arg{1};
-    switch state
-        case 0
-            rn = arg;
-            state = 1;
-        case 1
-            if isequal(arg,':')
-                state = 2;
-            elseif isequal(arg,';')
-                addrule(rn,{});
-                rn = ''; state = 0;
-            else
-                error('expecting : or ; in ar!');
-                s = -1;
-                return;
-            end
-        case 2
-            if isequal(arg,';')
-                addrule(rn,deplist);
-                rn = ''; deplist = {};
-                state = 0;
-            else
-                deplist{length(deplist)+1} = arg;
-            end
+short = 0;
+
+rn = varargin{1};
+for i = 2:nargin
+    a = varargin{i};
+    if a(1) == '#'
+        n = a(2:end);
+        fdlist{length(fdlist)+1} = n;
+    elseif a(1) == '@'
+        n = a(2:end);
+        deplist{length(deplist)+1} = n;
+    elseif a(1) == '!'
+        short = 1;
+        break;
+    elseif a(1) == ':'
+        break;
     end
 end
 
-if not(isequal(rn,''))
+pad = '';
+for j = (i+1):nargin
+    if strcmp(varargin{j},'$')
+        w = ';';
+    else
+        w = varargin{j};
+    end
+    rule = [rule pad w];
+    pad = ' ';
+end
+
+if short
     addrule(rn,deplist);
+else
+    addrule(rn,deplist,fdlist,rule);
 end
