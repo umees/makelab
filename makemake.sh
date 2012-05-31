a3b18bfbbd94282236a817bf7ecad15cdfa77829
s s <smschm@umich.edu>
2012-05-31 12:12:06 -0400

    ugh

--- makemake.sh
+++ makemake.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+if [ ! -f arch.h ]; then
+    echo "*******************************************";
+    echo "to build this, add a file called 'arch.h';"
+    echo "if on OSX, make it's contents simply:";
+    echo "#define OSX";
+    echo "otherwise, on Linux, leave it blank.";
+    echo "*******************************************";
+    exit;
+fi
+
 matlab -nojvm <<EOF > /dev/null 2> /dev/null
 f = fopen('/tmp/matlabroot','w');
 fprintf(f,'%s',matlabroot);
