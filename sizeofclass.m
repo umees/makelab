5424f5092c734b4156f418426888c172b5bb11df
s s <smschm@umich.edu>
2012-09-11 11:34:12 -0400

    making strings hashable

--- sizeofclass.m
+++ sizeofclass.m
@@ -3,7 +3,7 @@ function sz = sizeofclass(typestr)
 switch typestr
     case {'double', 'int64', 'uint64'}
         sz = 8;
-    case {'single', 'int32', 'uint32'}
+    case {'single', 'int32', 'uint32', 'char'}
         sz = 4;
     case {'int16', 'uint16'}
         sz = 2;
